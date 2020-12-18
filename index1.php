ef4880546dedcd8bcc9a8ce023c4fa9c99662f36
AmandaCP03 <acampezan@gmail.com>
2020-12-18 17:39:04 -0300

    Projeto 3 bim - permissoes

--- index1.php
+++ index1.php
@@ -8,7 +8,7 @@
 </head>
 <body>
     <h1>Página com links das minhas atividades</h1>
-    <a href="2B/trabalho_2Bim/index.php">Projeto 2o bimestre</a>
+    <a href="2B/trabalho_2Bim/index.php">Projeto 2o bimestre</a> <br/><br/>
     <a href="3B/revisao/index.php">Projeto 3o bimestre</a>
 </body>
 </html>
